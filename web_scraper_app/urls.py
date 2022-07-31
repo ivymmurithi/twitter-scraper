4ace9f6e44002631fefe7ab923d8d087ee7f9e91
Ivy Murithi <ivy.murithi@savannahinformatics.com>
2022-07-31 10:54:30 +0300

    change the url path from home/ to / so on app launching the homepage is displayed instead of a urls error

--- web_scraper_app/urls.py
+++ web_scraper_app/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('register/',views.register,name='register'),
     path('', include('django.contrib.auth.urls')),
-    path('home/', views.display_tweets, name='display_tweets'),
+    path('', views.display_tweets, name='display_tweets'),
     path('logoutuser',views.logoutuser, name='logoutuser'),
     path('like/<int:tweet_clicked_id>/', views.like_tweet, name='like'),
     path('detailed_view/<int:tweet_clicked_id>/', views.detailed_view, name='detailed_view'),
